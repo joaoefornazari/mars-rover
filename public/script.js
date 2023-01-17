233806e857bf6847afbc372bd26b90c66620bfd3
joaoefornazari <joaoefornazari@gmail.com>
2023-01-17 08:56:11 -0300

    Front-end methods applied.

---
+++ public/script.js
@@ -0,0 +1,51 @@
+/* LISTENERS */
+const roverData = {
+	moves: "",
+	initialPosition: {
+		x: 0,
+		y: 0,
+		direction: ""
+	}
+}
+
+function formatInput(input, text) {
+	if (text.length > 1) {
+		text = text[1]
+	}
+
+	if (input.id === "direction-position") {
+		if (!text.match(/N|S|W|E/)) text = 'N'
+	}
+
+	input.value = text
+}
+
+function addInitialPositionData(event, data) {
+	const input = event.target
+	let text = input.value
+
+	formatInput(input, text)
+
+	if (data === 'X') roverData.initialPosition.x = text
+	if (data === 'Y') roverData.initialPosition.y = text
+	if (data === 'DIR') roverData.initialPosition.direction = text
+}
+
+function addMove(move) {
+	roverData.moves += move
+	console.log(roverData.moves)
+
+	showMoveHistory()
+
+	const buttonSubmit = document.querySelector('.submit')
+	if (buttonSubmit.hasAttribute('disabled')) buttonSubmit.removeAttribute('disabled')
+}
+
+function showMoveHistory() {
+	const moveSequenceText = document.querySelector('#move-sequence')
+	moveSequenceText.textContent = roverData.moves
+}
+
+function sendMoves() {
+	
+}
